68c88842117cd2edf0b771e1c1f0f6a527a295db
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2024-04-22 16:28:21 +0200

    Create General.c

---
+++ General.c
@@ -0,0 +1,16 @@
+#include "General.h"
+
+// Variables globales
+
+// Funciones de trabajo
+
+//////////////////////////////
+// Funciones de manejo de registros
+
+//////////////////////////////
+// Funciones temporales 
+
+// Public interface for General library
+
+////////////////////////////
+// Interrupts handlers
