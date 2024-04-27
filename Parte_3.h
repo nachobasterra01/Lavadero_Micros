2076f182177bc62fcd792885ae0a70f0aa2b1051
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2024-04-27 20:29:29 +0200

    Update Parte_3.h

--- Parte_3.h
+++ Parte_3.h
@@ -2,8 +2,25 @@
 
 #ifndef PARTE_3_H_
 #define PARTE_3_H_
-/*
---Incluir macros necesarias
---void funciones();
-*/
+
+// Cinta de arrastre
+void setupCinta(void);
+void moveCinta(void); 
+void stopCinta(void);
+
+// LEDs (Estado maqueta y Semaforo)
+void setup_leds(void);
+void semaforo(); //LED_4_5
+void encendido_L1(void);
+void apagado_L1(void);
+void LED_1(void); // Pendiente (Consulta periódica)
+
+// Parada de emergencia
+void setup_ParadaEmergencia(void);
+void ParadaEmergencia(void); //Pendiente (Por interrupción)
+
+// Inicialización
+void setup_Parte3(void);
+
 #endif /* PARTE_3_H_ */
+
