f9c11a35003189382a7edc81229e9c721b5bce47
mfdezc <167860517+mfdezc@users.noreply.github.com>
2024-04-30 11:53:51 +0200

    Update Parte_2.c

--- Parte_2.c
+++ Parte_2.c
@@ -3,3 +3,11 @@
 // VARIABLES
 
 // FUNCIONES (Inicialización, Uso...)
+
+//interrupcion periodica para la luz
+
+//barrera
+//si SO2==1, BARRERA CERRADA
+    si SO1 detecta un coche, EN=1¿CONTANDO PULSOS?
+
+//lavado vertical
