514e4252c398b7084529f3b1fbb5b12fd1898216
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2024-05-02 03:50:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,3 +2,18 @@
 #include "Parte_1.h"
 #include "Parte_2.h"
 #include "Parte_3.h"
+
+int main(void) {
+    setup_Parte_1();
+	//setup_Parte_2();
+	//setup_Parte_3();
+	
+	while(1) {
+        if(!getStop()){
+			Parte_1();
+			//Parte_2();
+			//Parte_3();
+		}
+		
+    }
+}
