583a96e38cd2c783f8c8a6bfc26d221f5333d702
CarlosMR18 <103529858+CarlosMR18@users.noreply.github.com>
2024-05-02 02:14:04 +0200

    Update Parte_1.h

--- Parte_1.h
+++ Parte_1.h
@@ -1,10 +1,31 @@
-
 #include "General.h"
 
 #ifndef PARTE_1_H_
 #define PARTE_1_H_
-/*
---Incluir macros necesarias
---void funciones();
-*/
+
+// Lavadero Horizontal
+void setup_LavHorizontal(void);
+
+void up_LavHorizontal(void); // Altura rodillo
+void down_LavHorizontal(void);
+void stop_AlturaH(void);
+
+void on_LavHorizontal(void); // Giro rodillo
+void off_LavHorizontal(void);
+
+void lavaderoHorizontal(void); //Actualización Lavadero Horizontal
+
+// Secadero
+void setup_secado(void);
+
+void up_secado(void);
+void down_secado(void);
+void stop_secado(void);
+
+void secado(void); //Actualización Secado
+
+// Común Parte_1
+void setup_Parte1(void);
+void Parte_1(void);
+
 #endif /* PARTE_1_H_ */
