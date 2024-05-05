31e973b07c5bd2339b9e4ed98e315099f264c7f8
mfdezc <167860517+mfdezc@users.noreply.github.com>
2024-05-05 19:32:17 +0200

    Update Parte_2.h

--- Parte_2.h
+++ Parte_2.h
@@ -2,12 +2,12 @@
 
 #ifndef PARTE_2_H_
 #define PARTE_2_H_
-/*
+
 --Incluir macros necesarias
 --void funciones();
 void setup_luz(); 
 void luz(); 
 void setup_barrera(); 
 void barrera(); 
-*/
+
 #endif /* PARTE_2_H_ */
