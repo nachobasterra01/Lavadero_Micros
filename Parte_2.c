450b9766823d4faff2b2cea4eb75c44f1d68a707
mfdezc <167860517+mfdezc@users.noreply.github.com>
2024-05-05 19:31:23 +0200

    Update Parte_2.c

--- Parte_2.c
+++ Parte_2.c
@@ -4,6 +4,8 @@
 
 // FUNCIONES (Inicialización, Uso...)
 void setup_luz(){
+	DDRL=0X00; 
+	PORTL=0X00; 
 	cli();												//deshabilito las interrupciones globales
 	TCCR5A= 0x00;										//configurar CTC
 	TCCR5B= (1 << WGM52) | (1 << CS51) | (1 << CS50);	//Preescalador de 64
@@ -14,30 +16,44 @@ void setup_luz(){
 }
 
 
-// void parapdeoluz_0.5(){
-// 	if(s%30==0){
-// 		if(encendido==1){
-// 		        if(PORTB=0X01){
-// 				PORTB=0x00; 
-// 			} else 
-// 				PORTB=0X01; 
-// 		}
-// 	}
-// }
-//interrupcion periodica para la luz
-ISR( TIMER5_COMPA_vect ){	//interrupcion periodica cada 0.5s
-	if(encendido==1){
+void luz(){
+	if(s%30==0){
+		if(encendido==1){
 		        if(PORTB=0X01){
 				PORTB=0x00; 
 			} else 
 				PORTB=0X01; 
 		}
+	}
 }
+
+//interrupcion periodica para la luz
+// ISR( TIMER5_COMPA_vect ){	//interrupcion periodica cada 0.5s
+// 	if(encendido==1){
+// 		        if(PORTB=0X01){
+// 				PORTB=0x00; 
+// 			} else 
+// 				PORTB=0X01; 
+// 		}
+// }
+
+void setup_barrera(){
+	DDRK= 0X00;
+	PORTK=0X00;  
+}
+
+void barrera(){
+	if(PINL2==1){
+		PINK = (PINK2<<1); 
+	}
+}
+
+
 
 int dummy;
 int main(void)
 {
-
+	setup_barrera();
 	setup_luz();
 	/* Replace with your application code */
 	while (1)
